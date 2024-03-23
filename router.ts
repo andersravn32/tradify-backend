cb9854aafaad7cb197601c81a69a62210ddc26c4
Anders Ravn <71639005+andersravn32@users.noreply.github.com>
2024-03-23 21:56:12 +0100

    Added users route

--- router.ts
+++ router.ts
@@ -1,8 +1,11 @@
 import express from "express";
 import authRouter from "./controllers/auth";
+import usersRouter from "./controllers/users";
 
 const router = express.Router();
 
 router.use("/auth", authRouter);
+
+router.use("/users", usersRouter);
 
 export default router;
