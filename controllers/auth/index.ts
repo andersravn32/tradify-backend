54f9266565c231fc9180d019a0f9846e1a6afc89
Anders Ravn <71639005+andersravn32@users.noreply.github.com>
2024-03-24 10:50:16 +0100

    Applied callback handler

--- controllers/auth/index.ts
+++ controllers/auth/index.ts
@@ -1,11 +1,13 @@
 import express from "express";
-import refresh from "./refresh"
+import refresh from "./refresh";
+import callback from "./callback";
 import signup from "./email/signup";
 import signin from "./email/signin";
 
 const router = express.Router();
 
-router.post("/refresh", refresh)
+router.post("/refresh", refresh);
+router.get("/callback/:token", (req, res) => callback(req.params.token));
 
 router.post("/email/signup", signup);
 router.post("/email/signin", signin);
