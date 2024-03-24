c0bc26f4b91a151eaf8b602eb2537063acc2060c
Anders Ravn <71639005+andersravn32@users.noreply.github.com>
2024-03-24 10:33:13 +0100

    Added model export map

---
+++ models/index.ts
@@ -0,0 +1,5 @@
+export { default as Profile } from "./Profile";
+export { default as Rating } from "./Rating";
+export { default as Token } from "./Token";
+export { default as Trade } from "./Trade";
+export { default as User } from "./User";
