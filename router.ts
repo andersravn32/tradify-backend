e7949e15e8a13dfbaeebfbce662978248ebf7a62
Anders Ravn <71639005+andersravn32@users.noreply.github.com>
2024-03-24 10:34:37 +0100

    Applied auth middleware to /users and /trades endpoints

--- router.ts
+++ router.ts
@@ -2,13 +2,14 @@ import express from "express";
 import authRouter from "./controllers/auth";
 import usersRouter from "./controllers/users";
 import tradesRouter from "./controllers/trades";
+import Check from "./middleware/check";
 
 const router = express.Router();
 
 router.use("/auth", authRouter);
 
-router.use("/users", usersRouter);
+router.use("/users", Check.Auth, usersRouter);
 
-router.use("/trades", tradesRouter);
+router.use("/trades", Check.Auth, tradesRouter);
 
 export default router;
